b59f90407ce36151f3f452949f293a07466a0bf6
Supatarchai <Luck-y_se7en@hotmail.com>
2022-04-25 10:51:28 +0700

    Send Message

--- controller/messengerController.js
+++ controller/messengerController.js
@@ -1,4 +1,5 @@
 const User = require("../models/authModel");
+const messageModel = require("../models/messageModel");
 
 module.exports.getFriends = async (req, res) => {
   const myId = req.myId;
@@ -7,6 +8,34 @@ module.exports.getFriends = async (req, res) => {
     const friendGet = await User.find({});
     const filter = friendGet.filter((d) => d.id !== myId);
     res.status(200).json({ success: true, friends: filter });
+  } catch (error) {
+    res.status(500).json({
+      error: {
+        errorMessage: "Internal Sever Error",
+      },
+    });
+  }
+};
+
+module.exports.messageUploadDB = async (req, res) => {
+  const { senderName, reseverId, message } = req.body;
+
+  const senderId = req.myId;
+
+  try {
+    const insertMessage = await messageModel.create({
+      senderId: senderId,
+      senderName: senderName,
+      reseverId: reseverId,
+      message: {
+        text: message,
+        image: "",
+      },
+    });
+    res.status(201).json({
+      success: true,
+      message: insertMessage,
+    });
   } catch (error) {
     res.status(500).json({
       error: {
